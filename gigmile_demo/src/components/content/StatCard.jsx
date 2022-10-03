f197e8f64eaabb8709da535fe9229a69ce723749
Yaw Agyemang <48635382+madKNIGHTT@users.noreply.github.com>
2022-10-03 12:05:31 +0000

    Update StatCard.jsx

--- gigmile_demo/src/components/content/StatCard.jsx
+++ gigmile_demo/src/components/content/StatCard.jsx
@@ -4,9 +4,9 @@ const StatCard = ( {id,icon,amount,label} ) => {
   return (
     <div className='focus-within:border-[#1DB385] focus-within:outline-none focus-within:ring-0 border border-[##E6E7E9] hover:border-[#1DB385]/70 w-[156px] h-16 sm:h-[140px] rounded app__content-stats-item flex flex-auto md:flex-none items-center justify-center'>
         <button type='button' className='group focus:outline-none focus:ring-0 h-full text-center flex flex-auto items-center justify-center sm:flex-col'>
-            <div className='w-full h-5 basis-1/3 sm:w-[44px] sm:h-[44px] mx-auto'>
+            <div className='w-full h-5 basis-1/3 sm:w-[44px] sm:h-[44px] mx-auto flex items-center justify-center'>
                 {/* <FaClipboardCheck className='text-[#474F54]/30 group-hover:text-[#1DB385] w-full h-full' /> */}
-                {React.createElement(icon,{ id, className:'text-[#474F54]/30 group-hover:text-[#1DB385] group-focus:text-[#1DB385] w-full h-full object-cover' })}
+                {React.createElement(icon,{ id, className:'text-[#474F54]/30 group-hover:text-[#1DB385] group-focus:text-[#1DB385] w-full h-[34px]' })}
             </div>
 
             <div className='flex flex-col items-center sm:ml-0 w-full'>
